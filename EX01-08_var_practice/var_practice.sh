43ef652542ce81e1461c71529caa73cb2527ee89
takecareYata <ttrisq@gmail.com>
2026-09-18 16:51:05 +0900

    260918 수업내용

--- EX01-08_var_practice/var_practice.sh
+++ EX01-08_var_practice/var_practice.sh
@@ -8,11 +8,14 @@ var="$name $course"
 echo $var
 
 # $linux \expert
-
+var="\$$name \\$course"
+echo $var
 
 # $$linux \\expert
-
+var="\$\$$name \\\\$course"
+echo $var
 
 # experted linux
-
-
+var="${course}ed $name"
+#var=""$course"ed $name"
+echo $var
